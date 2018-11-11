658bd89971c061c0cc675f9f04e409dedc1d17c7
Thanos Papaoikonomou <thanos.papaoikonomou@gmail.com>
2018-11-11 17:08:13 +0200

    First push of unit tests

---
+++ tests/test_evaluators.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+import app.evaluators as ev
+
+
+def test_most_similar(mocker):
+
+    mock_np_load = mocker.patch('app.evaluators.np.load')
+    mock_np_load.return_value = np.array(
+        [
+            [1.0, 0.5, 0.2],
+            [0.5, 1.0, 0.7],
+            [0.2, 0.7, 1.0]
+        ]
+    )
+
+    mock_np_argsort = mocker.patch('app.evaluators.np.argsort')
+    mock_np_argsort.return_value = np.array([0, 2, 1])
+
+    mock_open = mocker.patch('app.evaluators.open')
+    mock_open.return_value = 'test'
+
+    mock_pickle_load = mocker.patch('app.evaluators.pickle.load')
+    mock_pickle_load.side_effect = [
+        {
+            'bla': 0,
+            'blah': 1,
+            'blahblah': 2
+         },
+
+        {
+            0: 'bla',
+            1: 'blah',
+            2: 'blahblah'
+        }
+    ]
+
+    res = ev.get_most_similar(word='blah', k=2)
+    assert len(res) == 2
+    assert res[0] == ('bla', 0.5)
+    assert res[1] == ('blahblah', 0.7)
+
+
+def test_most_uknown_word(mocker):
+
+    mock_np_load = mocker.patch('app.evaluators.np.load')
+    mock_np_load.return_value = np.array(
+        [
+            [1.0, 0.5, 0.2],
+            [0.5, 1.0, 0.7],
+            [0.2, 0.7, 1.0]
+        ]
+    )
+
+    mock_np_argsort = mocker.patch('app.evaluators.np.argsort')
+    mock_np_argsort.return_value = np.array([0, 2, 1])
+
+    mock_open = mocker.patch('app.evaluators.open')
+    mock_open.return_value = 'test'
+
+    mock_pickle_load = mocker.patch('app.evaluators.pickle.load')
+    mock_pickle_load.side_effect = [
+        {
+            'bla': 0,
+            'blah': 1,
+            'blahblah': 2
+         },
+
+        {
+            0: 'bla',
+            1: 'blah',
+            2: 'blahblah'
+        }
+    ]
+
+    res = ev.get_most_similar(word='aaaa', k=2)
+    assert len(res) == 0
